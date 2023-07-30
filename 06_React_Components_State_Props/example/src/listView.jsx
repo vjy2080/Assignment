593f52f034c528516e11522d0fe3171e4a1e2a1c
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-30 20:11:29 +0530

    Update Files

--- 06_React_Components_State_Props/example/src/listView.jsx
+++ 06_React_Components_State_Props/example/src/listView.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 
-
-export default function listView() {
+export default function ListView() {
   const [count, setCount] = useState(0);
-  const [increment, setIncrement] = useState(1);
-
-  function onTick() {
-	setCount(count + increment);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+  {
+    document.body.style.backgroundColor = "royalblue";
   }
 
-  useEffect(() => {
-    const id = setInterval(onTick, 1000);
-    return () => clearInterval(id);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   return (
     <>
       <div
@@ -25,19 +20,30 @@ export default function listView() {
         <div className="row align-items-center">
           <div className="col  ">
             <h1 className="text-center text-white">React Web</h1>
-            <h2 className="text-center">0</h2>
+            <h2 className="text-center my-5 fs-1">{count}</h2>
             <div className="text-center">
-              <Button variant="danger" className="mx-1 px-3 border">
+              <Button
+                variant="danger"
+                className="mx-3 px-3 border"
+                onClick={decrement}
+              >
                 Decrement
               </Button>
-              <Button variant="success" className="mx-1 px-3 border" disabled={increment === 0} onClick={() => {
-          setIncrement(i => i - 1);
-        }}>
+              <Button
+                variant="success"
+                className="mx-3 px-3 border"
+                onClick={increment}
+              >
                 Increment
               </Button>
             </div>
             <div className="text-center">
-              <Button variant="warning" className="my-3 px-3 border" onClick={() => setCount(0)}>
+              <Button
+                variant="secondary"
+                className="my-5 px-3 border"
+                onClick={reset}
+                disabled={count === 0}
+              >
                 Reset
               </Button>
             </div>
