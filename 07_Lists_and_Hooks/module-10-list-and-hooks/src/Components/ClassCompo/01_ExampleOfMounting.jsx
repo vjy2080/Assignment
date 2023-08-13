d295ef1e049b32b44125951659cddba875a14725
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-08-13 19:04:52 +0530

    Finalize assignment List & Hooks

--- 07_Lists_and_Hooks/module-10-list-and-hooks/src/Components/ClassCompo/01_ExampleOfMounting.jsx
+++ 07_Lists_and_Hooks/module-10-list-and-hooks/src/Components/ClassCompo/01_ExampleOfMounting.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class ExampleOfMounting extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { favoritecolor: "Red" };
+    this.state = { favoritecolor: "" };
   }
 
   componentDidMount() {
@@ -16,7 +16,7 @@ class ExampleOfMounting extends React.Component {
     return (
       <>
         <h1 className='my-5 text-center'>Example of "constructor" & "componentDidMount"</h1>
-        <p className='text-center'>componentDidMount after <strong>"3-seconds"</strong></p>
+        <p className='text-center'>componentDidMount after <strong>"3-seconds"</strong> "Blue"</p>
         <h3 className='mb-5 text-center'>My Favorite Color is &#10140; <span className='fs-1'>"{this.state.favoritecolor}"</span></h3>
         <hr className='border border-dark border-2 opacity-100' />
         <h2 className='my-3'>The Syntax is :- </h2>
@@ -24,7 +24,7 @@ class ExampleOfMounting extends React.Component {
           class ExampleOfMounting extends React.Component &#123; <br />
           &nbsp; constructor(props) &#123; <br />
           &nbsp; super(props); <br />
-          &nbsp; this.state = &#123; favoritecolor: "red" &#125;; <br />
+          &nbsp; this.state = &#123; favoritecolor: " " &#125;; <br />
           &#125; <br />
           componentDidMount() &#123; <br />
           &nbsp;&nbsp;setTimeout(() =&gt; &#123; <br />
@@ -34,7 +34,8 @@ class ExampleOfMounting extends React.Component {
           render() &#123; <br />
           &nbsp;&nbsp; return ( <br />
           &lt;&gt; <br />
-          &nbsp;&nbsp;&nbsp; &lt;h1&gt;Example of Constructor&lt;/h1&gt; <br />
+          &nbsp;&nbsp;&nbsp; &lt;h1&gt;Example of "Constructor" & "componentDidMount" &lt;/h1&gt; <br />
+          &nbsp;&nbsp;&nbsp; &lt;p className='text-center'&gt;componentDidMount after &lt;strong&gt;"3-seconds"&lt;/strong&gt; "Blue"&lt;/p&gt; <br />
           &nbsp;&nbsp;&nbsp; &lt;h3&gt;My Favorite Color is &#10140; &lt;span className='text-danger'&gt;"&#123;this.state.favoritecolor&#125;"&lt;/span&gt;&lt;/h3&gt; <br />
           &lt;/&gt; <br />
           &nbsp;&nbsp;&nbsp; ); <br />
