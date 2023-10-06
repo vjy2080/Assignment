792bc475172d0bb1ed3ed26ddc5b5ea7bce454ab
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-10-06 12:37:53 +0530

    Create New Assignment-10 Files

---
+++ 10_React - Applying Redux/react-applying-redux/src/Assignment10.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Assignment10.css'
+
+export default function Assignment10() {
+    return (
+        <>
+            <header>
+                <h1>Part 3 : ReactJs</h1>
+                <h2>Module-13 React - Applying Redux</h2>
+                <h3>Vijay G. Prajapati</h3>
+            </header>
+
+            <ol>
+                <hr class="hr" />
+                <li>Are the HTML tags and elements the same thing?</li>
+
+                <p><b>Answer :- </b>
+                    No,Because HTML tags are simple instructions that tell a web browser how to format text,and HTML
+                    Elements are the on-page content sandwiched within the tags.</p>
+
+                <hr />
+            </ol>
+        </>
+    );
+}
